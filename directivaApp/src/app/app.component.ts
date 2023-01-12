31a573b1c2b209ee0eeb1f3877c88b919249ad01
id4n4 <nagatiitoo@hotmail.com>
2023-01-12 18:03:06 -0500

    modify proyect directivas

--- directivaApp/src/app/app.component.ts
+++ directivaApp/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directivaApp';
+  titulo : string = 'Registro de usuarios'
+  nombre : string = ""
+  apellido : string = ""
+  registrado : boolean = false
+  mensaje : string = ""
+  entradas: Array<{titulo:string}>;
+
+  constructor(){
+    this.entradas = [
+      {titulo: "Python cada día más presente"},
+      {titulo: "Java presente desde hace más de 20 años"},
+      {titulo: "Javascript cada vez más funcional"},
+      {titulo: "Kotlin potencia para tus apps"},
+      {titulo: "¿Dónde quedó pascal? "}
+    ]
+  }
+
+
+  registrarUsuario(){
+    this.registrado = true
+    this.mensaje = "Usuario Registrado con éxito"
+  }
 }

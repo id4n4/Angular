1914f62d50149328683ed94c83ee139827d7e982
id4n4 <nagatiitoo@hotmail.com>
2023-01-12 19:27:51 -0500

    app-empleados finished

--- app-empleados/src/app/app.component.ts
+++ app-empleados/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  title = 'Listado de Empleados';
+  empleados : Empleado[] = [
+    new Empleado("Juan", "Diaz", "Presidente", 7500),
+    new Empleado("Maria","Valencia","Directora",7500),
+    new Empleado("Jose","Sanchez","Jefe de Sección",7000),
+    new Empleado("Andruth", "Polo", "Administrativo", 7000),
+  ]
+  inputName: string = "";
+  inputLastName: string = "";
+  inputJob: string = "";
+  inputSalary: number = 0;
+
+  agregarEmpleado(){
+    let miEmpleado = new Empleado(this.inputName, this.inputLastName, this.inputJob, this.inputSalary)
+    this.empleados.push(miEmpleado)
+  }
 }
